668de9607c236d06fb04b316fe46b7a38052ca1d
AnaCarolinaAndrade <ana_ca_araujo@estudante.sesisenai.org.br>
2024-09-26 17:28:26 -0300

    Site da márcia

---
+++ farmacia/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Nav_bar from './Components/Nav_bar'
+
+function App() {
+
+  return (
+    <>
+      <Nav_bar/>
+
+    </>
+  )
+}
+
+export default App
---
+++ farmacia/src/Components/Nav_bar.jsx
@@ -0,0 +1,16 @@
+import "./Nav_bar.css"
+
+function Nav_bar() {
+  return (
+    <div className='container_nav'>
+        <img src="./Imagens/farmácia.png"/>
+      <a>Home</a>
+      <a>Sobre nós</a>
+      <a>catalogo</a>
+      <a>Entrar</a>
+    </div>
+  )
+}
+
+export default Nav_bar
+.
